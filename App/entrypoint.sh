edffe1d09ff9e6995fb95ce232ed9b9cbd2a8d25
JongPil Lee <jplee@langcode.io>
2025-03-10 12:10:38 +0900

    Dockerfile 및 entrypoint.sh 경로 수정
    Dockerfile에서 모듈의 .csproj 파일 경로를 변경하고, entrypoint.sh의 복사 경로를 수정하여 실행 권한을 부여했습니다. 또한, entrypoint.sh에서 데이터베이스 마이그레이션 명령어의 경로를 업데이트했습니다.

--- App/entrypoint.sh
+++ App/entrypoint.sh
@@ -1,8 +1,4 @@
 ﻿#!/bin/sh
 
-echo "Applying migrations..."
-dotnet ef database update --context TodoDbContext --project Modules/Todo/Todo --startup-project src/App
-dotnet ef database update --context UserDbContext --project Modules/User/User --startup-project src/App
-
 echo "Starting application..."
 exec dotnet App.dll
